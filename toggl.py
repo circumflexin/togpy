08f148674b610e8fcac027594cad94d3faedabf9
Alec Burslem <alec.burslem@gmail.com>
2016-08-04 08:58:54 +0100

    initial commit

---
+++ toggl.py
@@ -0,0 +1,21 @@
+
+def getd(user, token, start = None , stop = None, client = None):
+	url = "https://toggl.com/reports/api/v2/details?workspace_id=%i&user_agent=alec.burslem@gmail.com" % user
+	if start != None:
+		url = url + "&since=%s" % start
+	if stop != None:
+		url = url + "&until=%s" % stop
+	dets = requests.get(url, auth =(token, 'api_token'),)
+	dets = dets.json()
+	pages = [dets]
+	if dets["total_count"] > dets["per_page"]:
+		reps = dets["total_count"] / dets["per_page"]
+		count=1
+		for page in range(1, math.ceil(reps)):
+			nurl = url + '&page=%i' % page
+			data = requests.get(nurl, auth =(token, 'api_token'),)
+			data = data.json()
+			pages.append(data)
+			count += 1
+	return pages
+
